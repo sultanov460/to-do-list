0353b1166d29a6d81a485b7e4558cac4b0eec888
Rafiq Sultanov <sultanovrafiq71@gmail.com>
2025-06-19 10:03:51 +0400

    Add localstorage

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const todoInput = document.getElementById("input");
 const todoList = document.getElementById("list");
 
 function init() {
+  const saved = JSON.parse(localStorage.getItem("todos")) || [];
+  saved.forEach((task) => {
+    const todoItem = createTodoItem(task);
+    todoList.append(todoItem);
+  });
   addBtn.addEventListener("click", handleAdd);
   todoInput.addEventListener("keypress", (event) => {
     if (event.key == "Enter") handleAdd();
@@ -11,13 +16,15 @@ function init() {
 
 function handleAdd() {
   const task = todoInput.value.trim();
-  if (!task && task.length < 5) {
+  if (!task || task.length < 5) {
     alert("Your to-do is too short");
     return;
   }
 
   const todoItem = createTodoItem(task);
   todoList.append(todoItem);
+
+  saveTodo(task);
   clearInput();
 }
 
@@ -36,12 +43,26 @@ function createTodoItem(text) {
 
   deleteBtn.addEventListener("click", () => {
     li.remove();
+
+    deleteTodo(task);
   });
   return li;
 }
 
 function clearInput() {
   todoInput.value = "";
+}
+
+function saveTodo(task) {
+  const todos = JSON.parse(localStorage.getItem("todos")) || [];
+  todos.push(task);
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function deleteTodo(task) {
+  let todos = JSON.parse(localStorage.getItem("todos")) || [];
+  todos = todos.filter((t) => t !== task);
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
 
 init();
